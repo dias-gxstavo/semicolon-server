33ccb2a37afd38ef07852f232f69ffbe1a06d852
Gustavo Dias <gustavodiashug9@gmail.com>
2026-08-29 12:16:11 -0300

    feat: adding the NoteUpdate schema and improving the structure of model_config

--- src/schemas/note.py
+++ src/schemas/note.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
 
 
 class NoteCreate(BaseModel):
@@ -6,10 +8,15 @@ class NoteCreate(BaseModel):
     content: str
 
 
-class NoteResponse(BaseModel):
-    note_id: int
+class NoteUpdate(BaseModel):
     title: str
     content: str
 
-    class Config:
-        from_attributes = True
+
+class NoteResponse(BaseModel):
+    note_id: int
+    title: str
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
