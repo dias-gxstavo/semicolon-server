b0dec8fcff457703ac767d5fdeda5d8c2a4a0f56
Gustavo Dias <gustavodiashug9@gmail.com>
2026-09-02 15:12:29 -0300

    docs: improving the readability of the documentation

--- src/main.py
+++ src/main.py
@@ -7,13 +7,16 @@ from .models import note
 from .routers import notes
 
 note.Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(title='semicolon - a simple markdown editor')
 
 app.include_router(notes.router)
 
 
 @app.get('/health', status_code=status.HTTP_200_OK, tags=['health'])
 async def health_check(db: Session = Depends(get_db)):
+    """
+    Returns the status of the database connection
+    """
     try:
         db.execute(text('SELECT 1'))
         return {'status': 'healthy', 'database': 'connected'}
